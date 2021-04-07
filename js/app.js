d415494b6b74df5d63fd3ad4d2b1fabd90be6462
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-04-07 09:08:14 -0500

    Modified programs section changed slider from lightslider to bootstrap. resposive styles to home and about pages.

--- js/app.js
+++ js/app.js
@@ -77,17 +77,7 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-// programs carousel
 
-$(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        } 
-    });  
-  });
 
   // tabs
 
@@ -111,4 +101,54 @@ $(document).ready(function() {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+  // NEW PROGRAMS SLIDER
+
+  $(document).ready(function()
+{
+
+
+if($('.bbb_viewed_slider').length)
+{
+var viewedSlider = $('.bbb_viewed_slider');
+
+viewedSlider.owlCarousel(
+{
+loop:true,
+margin:30,
+autoplay:true,
+autoplayTimeout:6000,
+nav:false,
+dots:false,
+responsive:
+{
+0:{items:1},
+575:{items:2},
+768:{items:3},
+991:{items:4},
+1199:{items:6}
+}
+});
+
+if($('.bbb_viewed_prev').length)
+{
+var prev = $('.bbb_viewed_prev');
+prev.on('click', function()
+{
+viewedSlider.trigger('prev.owl.carousel');
+});
+}
+
+if($('.bbb_viewed_next').length)
+{
+var next = $('.bbb_viewed_next');
+next.on('click', function()
+{
+viewedSlider.trigger('next.owl.carousel');
+});
+}
+}
+
+
+});
   
